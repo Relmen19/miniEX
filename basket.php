42dfca78c64809a475b3414a9ec1c29757c56ecd
Relmen19 <andrew.avramencko2104@gmail.com>
2019-02-11 09:27:47 +0300

    full basket

--- basket.php
+++ basket.php
@@ -6,17 +6,16 @@
 
         $link = mysqli_connect('localhost','root','','29092018_2_3project');
         mysqli_set_charset($link,'utf8');
-
+        $card = [];
         foreach( $_SESSION['basket']['products'] as $product_item ){
             $product_id = $product_item['id'];
             
             $sql = " SELECT * FROM products WHERE id = {$product_id} ";
-
-        };
-        $result = mysqli_query($link, $sql);
-        $card = [];
-        while( $row = mysqli_fetch_assoc($result) ){
-            $card[] = $row;
+            $result = mysqli_query($link, $sql);
+            
+            while( $row = mysqli_fetch_assoc($result) ){
+                $card[] = $row;
+            };
         };
         
     }else{
@@ -37,24 +36,39 @@
     <title>Вaша корзина</title>
 </head>
 <body>
+    
+<div class="wrapper">
     <div class="catalog">
+        <div class="header">
+            <a href="catalog.php" class="basket">Вернуться к каталогу</a>
+        </div>
         <div class="catalog-products">
             <?php
             
-            // foreach( $card as $item ){
-            //     echo "<img src='/images/catalog/{$item['photo']}'>
-            //     <div class='name'>{$item['name']}</div>
-            //     <div class='price'>{$item['price']} руб.</div>";
-            // };
-            
-            echo "<pre>";
-            print_r( $card );                                          
-            echo "</pre>";
-
+                foreach( $card as $item ){
+                    $img_src = 'default.jpeg';
+                    if  ($item['photo'] != '' ){
+                        $img_src = $item['photo'];
+                    };
+                    echo "
+                    <div class='catalog-product-item'>
+                        <img src='/images/catalog/{$img_src}'>
+                        <div class='name'>{$item['name']}</div>
+                        <div class='price'>{$item['price']} руб.</div>
+                    </div>
+                    ";
+                };
 
             ?>
         </div>
     </div>
+</div>
+
+
+
+
+           
+      
 
     
 
